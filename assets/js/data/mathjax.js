575690b074ba0a28065b83f241538511c6821d9b
hxf0223 <hxf0223@hotmail.com>
2026-01-19 16:21:15 +0800

    update: add MathJax support

---
+++ assets/js/data/mathjax.js
@@ -0,0 +1,34 @@
+---
+layout: compress
+# WARNING: Don't use '//' to comment out code, use '{% comment %}' and '{% endcomment %}' instead.
+---
+
+{%- comment -%}
+  See: <https://docs.mathjax.org/en/latest/options/input/tex.html#tex-options>
+{%- endcomment -%}
+
+MathJax = {
+  tex: {
+    {%- comment -%} start/end delimiter pairs for in-line math {%- endcomment -%}
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)']
+    ],
+    {%- comment -%} start/end delimiter pairs for display math {%- endcomment -%}
+    displayMath: [
+      ['$$', '$$'],
+      ['\\[', '\\]']
+    ],
+    {%- comment -%} equation numbering {%- endcomment -%}
+    tags: 'ams',
+    {%- comment -%} Load extensions {%- endcomment -%}
+    packages: { '[+]': ['ams', 'base', 'newcommand', 'configmacros'] }
+  },
+  options: {
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  },
+  loader: {
+    load: ['ui/lazy']
+  }
+};
