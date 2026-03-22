37930a783ddb43d3685fd7b978c299d05ba13606
hxf0223 <hxf0223@hotmail.com>
2026-03-22 04:00:49 +0000

    Deploying to gh-pages from @ hxf0223/hxf0223.github.io@f9e6005bc633ae5ee58208469a701f2daab2f9c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -441,6 +441,17 @@ ninja.data = [{
             window.location.href = "/blog/2025/Cute%E5%88%9D%E6%AD%A55-TV-Layout/";
           
         },
+      },{id: "post-cutlass-cute-初步-4-1-mma-swizzle-mma-ldmatrix-smem-swizzle",
+        
+          title: "CUTLASS-Cute 初步(4.1)：MMA Swizzle -- MMA、ldmatrix、smem swizzle",
+        
+        description: "ldmatrix 指令 &amp;amp; HMMA 指令一些名词： LDS：LoaD Shared Memory，warp 指令，比如LDS.32 表示加载 32 位数据到寄存器 LDSM：LoaD Shared Memory Matrix，Tensor Core 指令，ldmatrix的 SASS 表示ldmatrix指令为配合 Tensor Core 的 HMMA 指令使用的，数据在SMEM中的布局与mma指令一致。ldmatrix指令格式为：ldmatrix.sync.aligned.m8n8.x4.shared.b16{.trans} { %0, %1, %2, %3 }, [ %4 ]一个ldmatrix.x1指令加载一个8x8-BF16 = 128B矩阵，占用8个线程（比如0~7），并将从SMEM中加载的数据均分到一个warp 32个线程中，在warp线程中以32位寄存器存储。格式如下： ldmatrix.x1要求提供8 x 8-BF16 SMEM地址（每个线程提供一个，共8个线程），且每个地址16B且连续。 REG均分规律：T0 SMEM（16B = 8-BF16 = 4-REG） =&amp;gt; T0、T1、T2、T3；T1 SMEM =&amp;gt; T4、T5、T6、T7；…；T7 SMEM =&amp;gt; T28、T29、T30、T31。ldmatrix.x4指令加载一个32x8-BF16 = 512B矩阵，占用一个warp 32个线程，每个线程提供一个地址。每个线程提供4-REG = 8-BF16，对应到上述给出的ldmatrix.x4指令格式。图示如下：1.1....",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/Cute%E5%88%9D%E6%AD%A54.1-mma-ldmatrix-swizzle/";
+          
+        },
       },{id: "post-cutlass-cute-初步-4-swizzle",
         
           title: "CUTLASS-Cute 初步(4)：Swizzle",
